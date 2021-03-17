7812f593647dabc5e5723c8bc9c9c684d1eb9397
singhg6 <singhg6@BY2027A98607883.wit.private>
2021-03-17 09:10:57 -0400

    first part of main -gurjunt

--- tictactoe/src/tictactoe/tictactoe.java
+++ tictactoe/src/tictactoe/tictactoe.java
@@ -1,10 +1,46 @@
 package tictactoe;
 import java.util.Scanner;
 public class tictactoe {
+	     public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		//main method12
-		
-	}
 
+	         System.out.println("Welcome to Tickle Tackle Toe!!! :D");
+	         System.out.println();
+
+	         String marker1 = "x";
+	         String marker2 = "o";
+	         boolean playAgain = true;
+
+	         Scanner s = new Scanner(System.in);
+
+
+
+	         player1 player1 = new player1();
+	         computer computer = new computer();
+
+	         while(playAgain){
+	             set Setup = new set();
+
+	             Setup.createBoard();
+	             Setup.printBoard();
+
+	             System.out.println("please choose your marker");
+	             System.out.println("type 1 for 'x' or 2 for 'o'");
+
+	             //set markers
+	             if(s.nextInt() == 1){
+	                 // create player objects
+	                 player1.setMarker("x");
+	                 computer.setMarker("o");
+	             } 
+	             else
+	             {
+	                 player1.setMarker("o");
+	                 computer.setMarker("x");
+
+	             }
+
+				
+	         }
+	     }
 }
